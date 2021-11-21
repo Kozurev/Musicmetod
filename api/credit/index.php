75a92422bb9bf4b7c8b07b26b5da1433fba077c9
Egor <creative27016@gmail.com>
2021-11-21 15:12:38 +0300

    Отключена возможность рассрочки

--- api/credit/index.php
+++ api/credit/index.php
@@ -9,41 +9,43 @@ $action = request()->get('action');
  * Создание кредитной заявки
  */
 if ($action === 'createOrder') {
-    $tariffId = request()->get('tariff_id');
+    exit(REST::responseError(REST::ERROR_CODE_CUSTOM, 'Рассрочка недоступна на данный момент'));
 
-    if (empty($tariffId)) {
-        exit(REST::responseError(REST::ERROR_CODE_REQUIRED_PARAM, 'Отсутствует обязательный параметр tariff_id'));
-    }
-
-    $facade = new CreditServiceProvider();
-    $user = User_Client::find(User_Auth::current()->getId());
-    /** @var Payment_Tariff|null $tariff */
-    $tariff = Payment_Tariff::query()
-        ->where('id', '=', $tariffId)
-        ->where('subordinated', '=', User_Auth::current()->getDirector()->getId())
-        ->where('access', '=', 1)
-        ->find();
-
-    if (is_null($user)) {
-        exit(REST::responseError(REST::ERROR_CODE_AUTH, 'Покупка тарифов доступна только клиентам'));
-    }
-    if (is_null($tariff)) {
-        exit(REST::responseError(REST::ERROR_CODE_NOT_FOUND, 'Тариф с id ' . $tariffId . ' не найден'));
-    }
-
-    try {
-        $providerResponse = $facade->getProvider()->createOrder($user, $tariff);
-        if (isset($providerResponse->link)) {
-            $response = json_encode(['link' => $providerResponse->link]);
-        } else {
-            \Log::instance()->error('tinkoff', json_encode($providerResponse));
-            $response = REST::responseError(REST::ERROR_CODE_CUSTOM, 'Неизвестная ошибка');
-        }
-    } catch (\Throwable $throwable) {
-        $response = REST::responseError(REST::ERROR_CODE_CUSTOM, $throwable->getMessage());
-    }
-
-    exit($response);
+//    $tariffId = request()->get('tariff_id');
+//
+//    if (empty($tariffId)) {
+//        exit(REST::responseError(REST::ERROR_CODE_REQUIRED_PARAM, 'Отсутствует обязательный параметр tariff_id'));
+//    }
+//
+//    $facade = new CreditServiceProvider();
+//    $user = User_Client::find(User_Auth::current()->getId());
+//    /** @var Payment_Tariff|null $tariff */
+//    $tariff = Payment_Tariff::query()
+//        ->where('id', '=', $tariffId)
+//        ->where('subordinated', '=', User_Auth::current()->getDirector()->getId())
+//        ->where('access', '=', 1)
+//        ->find();
+//
+//    if (is_null($user)) {
+//        exit(REST::responseError(REST::ERROR_CODE_AUTH, 'Покупка тарифов доступна только клиентам'));
+//    }
+//    if (is_null($tariff)) {
+//        exit(REST::responseError(REST::ERROR_CODE_NOT_FOUND, 'Тариф с id ' . $tariffId . ' не найден'));
+//    }
+//
+//    try {
+//        $providerResponse = $facade->getProvider()->createOrder($user, $tariff);
+//        if (isset($providerResponse->link)) {
+//            $response = json_encode(['link' => $providerResponse->link]);
+//        } else {
+//            \Log::instance()->error('tinkoff', json_encode($providerResponse));
+//            $response = REST::responseError(REST::ERROR_CODE_CUSTOM, 'Неизвестная ошибка');
+//        }
+//    } catch (\Throwable $throwable) {
+//        $response = REST::responseError(REST::ERROR_CODE_CUSTOM, $throwable->getMessage());
+//    }
+//
+//    exit($response);
 }
 
 /**
