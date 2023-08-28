e00baf114700011c683ed6a60d2db04f9dae88a0
Egor Kozurev <creative27016@gmail.com>
2023-08-28 18:18:29 +0300

    composer dump-autoload

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -284,8 +284,8 @@ class ComposerStaticInit4b04af986000ff6718e1d59d2084dac3
         ),
         'Illuminate\\Support\\' => 
         array (
-            0 => __DIR__ . '/..' . '/illuminate/macroable',
-            1 => __DIR__ . '/..' . '/illuminate/collections',
+            0 => __DIR__ . '/..' . '/illuminate/collections',
+            1 => __DIR__ . '/..' . '/illuminate/macroable',
             2 => __DIR__ . '/..' . '/illuminate/support',
         ),
         'Illuminate\\Contracts\\' => 
